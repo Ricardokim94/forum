24e0bad6e03b50d48d76dc50a0b165191c6fdcc6
Ricardokim94 <cmrikim@naver.com>
2024-06-27 12:56:37 +0900

    글의 상세 (detail)페이지 (강제로 일단)

--- app/list/page.js
+++ app/list/page.js
@@ -8,6 +8,7 @@ export default async function List() {
     //await이란 처리가 늦게 되는 코드를 잠깐 기다려 주는 코드임. mongoDB에서 사용하는 것
     console.log('tttt', result)
     return (
+        // 반복문으로 게시글 contents 돌리기
         <div className="list-bg">
             {result.map((post, index) => (
                 <div className="list-item" key={index}>
@@ -16,18 +17,6 @@ export default async function List() {
                 </div>
             ))}
             
-            {/* <div className="list-item">
-                <h4>{result[0].title}</h4>
-                <p>{result[0].content}</p>
-            </div>
-            <div className="list-item">
-                <h4>{result[1].title}</h4>
-                <p>{result[1].content}</p>
-            </div>
-            <div className="list-item">
-                <h4>{result[2].title}</h4>
-                <p>{result[2].content}</p>
-            </div> */}
         </div>
     )
 } 
