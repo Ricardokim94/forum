40330f6e189d7e1458df04f963e14802b83d6c56
Ricardokim94 <cmrikim@naver.com>
2024-06-28 16:15:25 +0900

    바뀐 환경 있어서 test

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -11,7 +11,7 @@ export default async function Detail(props){
     return(
         <div>
             <h4>상세페이지</h4>
-            <h4>{result.title}</h4>
+            <h4>{result.title}</h4> 
             <p>{result.content}</p>
         </div>
     )
